7b351237ca6a021209a57afb7b6298513ab73117
lvan-sav <vansav.ka@gmail.com>
2023-03-15 15:16:20 +0200

    Chfnge timeouts

--- tests/main.page.spec.ts
+++ tests/main.page.spec.ts
@@ -4,36 +4,36 @@ import { test, expect } from '@playwright/test';
 test.describe('Main page', () => {
   test.only('Match snapshots on the Main page', async ({ page }) => {
     await page.goto('/')
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     expect(await page.screenshot()).toMatchSnapshot()
     
     const wantErmBtn = page.locator('.button[href*="/enterprise-risk-management/"]')
     await wantErmBtn.scrollIntoViewIfNeeded()
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     await expect(wantErmBtn).toBeInViewport()
     expect(await page.screenshot()).toMatchSnapshot()
 
     const wantAutoBtn = page.locator('.button[href*="/third-party-risk-management/"]')
     await wantAutoBtn.scrollIntoViewIfNeeded()
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     expect(wantAutoBtn).toBeInViewport()
     expect(await page.screenshot()).toMatchSnapshot()
 
     const firstArticle = page.locator('article.it-security-risk')
     await firstArticle.scrollIntoViewIfNeeded()
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     expect(firstArticle).toBeInViewport()
     expect(await page.screenshot()).toMatchSnapshot()
 
     const signUpBtn = page.locator('section#form')
     await signUpBtn.scrollIntoViewIfNeeded()
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     expect(signUpBtn).toBeInViewport()
     expect(await page.screenshot()).toMatchSnapshot()
 
     const reqDemoFooterBtn = page.locator('.bluedeep[href*="request-a-demo"]')
     await reqDemoFooterBtn.scrollIntoViewIfNeeded()
-    await page.waitForLoadState('networkidle')
+    await page.waitForTimeout(2000)
     expect(reqDemoFooterBtn).toBeInViewport()
     expect(await page.screenshot()).toMatchSnapshot()
   })
