ed50482824de7724942a42a5b23e3df097895fc6
lvan-sav <vansav.ka@gmail.com>
2023-03-15 13:16:30 +0200

    Change timeouts

--- config/playwright.config.ts
+++ config/playwright.config.ts
@@ -12,20 +12,20 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: '../tests',
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 50000
+    timeout: 10000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
