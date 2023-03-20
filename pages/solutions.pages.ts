19bc3c4ed0be6c929a776b05d8c3b9b05476139e
lvan-sav <vansav.ka@gmail.com>
2023-03-20 16:08:04 +0200

    Add security page and update ui test

--- pages/solutions.pages.ts
+++ pages/solutions.pages.ts
@@ -9,20 +9,50 @@ const policyManagmentUrl = '/solutions/policy-management/'
 const regManagmentUrl = '/solutions/regulatory-compliance-management/'
 const dataPrivacyUrl = '/solutions/data-privacy/'
 const businessManagementUrl = '/solutions/business-continuity-management/'
+const reqDemoFooterBtnLoc = '.bluedeep[href*="request-a-demo"]'
+const footerMenuLoc = 'ul#menu-footer-menu'
 
 export class SolutionPage extends BasePage {
-  async goto(page: string) {
+
+  firstSection = this.page.locator('section.hero .lazyloaded')
+  lastForm = this.page.locator('section#form')
+  lastFormImg = this.lastForm.locator('img.lazyloaded')
+  reqDemoFooterBtn = this.page.locator(reqDemoFooterBtnLoc)
+  footerMenu = this.page.locator(footerMenuLoc)
+
+  async goto(page: 
+    "security"
+    |"enterpriseRisk"
+    |"thirdPartyRisk"
+    |"auditControl"
+    |"policyManagement"
+    |"regManagement"
+    |"dataPrivacy"
+    |"businessManagement"
+    ) {
     const pages = {
       security: securityUrl,
       enterpriseRisk: enterpriseRiskUrl,
       thirdPartyRisk: thirdPartyRiskUrl,
       auditControl: auditControlsUrl,
-      policyManagment: policyManagmentUrl,
-      regManagment: regManagmentUrl,
+      policyManagement: policyManagmentUrl,
+      regManagement: regManagmentUrl,
       dataPrivacy: dataPrivacyUrl,
-      businessManagment: businessManagementUrl
+      businessManagement: businessManagementUrl
     }
 
     await this.page.goto(pages[page])
   }
+}
+
+export class SecurityPage extends SolutionPage {
+  streamlineBlockImg = this.page.locator('.item:nth-child(1) .lazyloaded')
+  assetsBlockImg = this.page.locator('.item:nth-child(2) .lazyloaded')
+  mitigationBlockImg = this.page.locator('.item:nth-child(3) .lazyloaded')
+  contextBlockImg = this.page.locator('.item:nth-child(4) .lazyloaded')
+  financialBlockImg = this.page.locator('.item:nth-child(5) .lazyloaded')
+  firstToolboxImg = this.page.locator('section.tools .ft-1 .lazyloaded')
+  firstAppArticle = this.page.locator('section.pad-top-std article:nth-child(1)')
+  firstResourcesArticleImg = this.page.locator('section.resources article:nth-child(1) .lazy')
+  secResourcesArticleImg = this.page.locator('section.resources article:nth-child(2) .lazy')
 }
--- tests/solution.dropdown.spec.ts
+++ tests/solution.dropdown.spec.ts
@@ -1,20 +1,57 @@
 import { test, expect } from '@playwright/test';
-import { SolutionPage } from '../pages/solutions.pages'
+import { 
+  SecurityPage
+ } from '../pages/solutions.pages'
 
 
 test.describe('Solution dropdown', () => {
-  test('Match snapshots on the Cyber risk page', async ({ page }) => {
-    const solutionPage = new SolutionPage(page)
-    
-    await solutionPage.goto('security')
-    await page.waitForLoadState('networkidle')
+  test.only('Match snapshots on the Cyber risk page', async ({ page }) => {
+    const securityPage = new SecurityPage(page)
+
+    await securityPage.goto('security')
+    await securityPage.firstSection.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 12; i++) {
-      await solutionPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await securityPage.streamlineBlockImg.scrollIntoViewIfNeeded()
+    await securityPage.streamlineBlockImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.assetsBlockImg.scrollIntoViewIfNeeded()
+    await securityPage.assetsBlockImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.mitigationBlockImg.scrollIntoViewIfNeeded()
+    await securityPage.mitigationBlockImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.contextBlockImg.scrollIntoViewIfNeeded()
+    await securityPage.contextBlockImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.financialBlockImg.scrollIntoViewIfNeeded()
+    await securityPage.financialBlockImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.firstToolboxImg.scrollIntoViewIfNeeded()
+    await securityPage.firstToolboxImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.firstAppArticle.scrollIntoViewIfNeeded()
+    await securityPage.firstAppArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.firstResourcesArticleImg.scrollIntoViewIfNeeded()
+    await securityPage.firstResourcesArticleImg.waitFor()
+    await securityPage.secResourcesArticleImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.lastForm.scrollIntoViewIfNeeded()
+    await securityPage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await securityPage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await securityPage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshots on the Enterprise risk page', async ({ page })  => {
@@ -62,7 +99,7 @@ test.describe('Solution dropdown', () => {
   test('Match snapshots on the Policy page', async ({ page }) => {
     const solutionPage = new SolutionPage(page)
 
-    await solutionPage.goto('policyManagment')
+    await solutionPage.goto('policyManagement')
     await page.waitForLoadState('networkidle')
     expect(await page.screenshot()).toMatchSnapshot()
 
@@ -76,7 +113,7 @@ test.describe('Solution dropdown', () => {
   test('Match snapshots on the Regulatory page', async ({ page }) => {
     const solutionPage = new SolutionPage(page)
 
-    await solutionPage.goto('regManagment')
+    await solutionPage.goto('regManagement')
     await page.waitForLoadState('networkidle')
     expect(await page.screenshot()).toMatchSnapshot()
 
@@ -104,7 +141,7 @@ test.describe('Solution dropdown', () => {
   test('Match snapshot on the Operational Resiliency page', async ({ page }) => {
     const solutionPage = new SolutionPage(page)
 
-    await solutionPage.goto('businessManagment')
+    await solutionPage.goto('businessManagement')
     await page.waitForLoadState('networkidle')
     expect(await page.screenshot()).toMatchSnapshot()
 
