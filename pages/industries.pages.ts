246a070ce5c2a1bbe88f369f9a10b0e8d5f31cc2
lvan-sav <vansav.ka@gmail.com>
2023-03-21 18:20:15 +0200

    industries tests update and pom

--- pages/industries.pages.ts
+++ pages/industries.pages.ts
@@ -9,7 +9,27 @@ const riskManagementUrl = '/industries/risk-management-software-for-banks/'
 const insuranceUrl = '/industries/insurance/'
 
 export class IndustriesPage extends BasePage {
-  async goto(page: string) {
+
+  pageTitle = this.page.locator('section.hero')
+  lastForm = this.page.locator('section#form')
+  lastFormImg = this.lastForm.locator('img.lazyloaded')
+  reqDemoFooterBtn = this.page.locator('.bluedeep[href*="request-a-demo"]')
+  footerMenu = this.page.locator('ul#menu-footer-menu')
+  firstContentArticle = this.page.locator('section.maincontent .item:nth-child(2) .lazyloaded')
+  secContentArticle = this.page.locator('section.maincontent .item:nth-child(3) .lazyloaded')
+  thirdContentArticle = this.page.locator('section.maincontent .item:nth-child(4) .lazyloaded')
+  firstSolutionBlock = this.page.locator('article.solution:nth-child(1) .lazyloaded')
+  firstResourcesBlock = this.page.locator('section.resources article:nth-child(1) .lazy')
+  infoBlock = this.page.locator('section.offset-top')
+
+  async goto(page: 
+    "industries"|
+    "software"|
+    "fintech"|
+    "telecom"|
+    "riskManagement"|
+    "insurance"
+    ) {
     const pages = {
       industries: industriesUrl,
       software: softwareUrl,
@@ -22,3 +42,20 @@ export class IndustriesPage extends BasePage {
     await this.page.goto(pages[page])
   }
 }
+
+export class IndustriesOwerviewPage extends IndustriesPage {
+  softwareArticle = this.page.locator('article.industry:nth-child(1) .lazy')
+  finTechArticle = this.page.locator('article.industry:nth-child(2) .lazy')
+  hospitalsArticle = this.page.locator('article.industry:nth-child(3) .lazy')
+  investimentArticle = this.page.locator('article.industry:nth-child(7) .lazy')
+}
+
+export class SoftwarePage extends IndustriesPage {}
+
+export class FinTechPage extends IndustriesPage {}
+
+export class TelecomPage extends IndustriesPage {}
+
+export class BankingPage extends IndustriesPage {}
+
+export class InsurancePage extends IndustriesPage {}
--- tests/Industries.dropdown.spec.ts
+++ tests/Industries.dropdown.spec.ts
@@ -1,83 +1,222 @@
 import { test, expect } from '@playwright/test';
-import { IndustriesPage } from '../pages/industries.pages';
+import { 
+  IndustriesOwerviewPage,
+  SoftwarePage,
+  FinTechPage,
+  TelecomPage,
+  BankingPage,
+  InsurancePage
+ } from '../pages/industries.pages';
 
 
 test.describe('Industries pages', () => {
   test('Match snapshots on the Industries page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
+    const industriesPage = new IndustriesOwerviewPage(page)
+    
     await industriesPage.goto('industries')
-    await page.waitForLoadState('networkidle')
+    await industriesPage.pageTitle.waitFor()
+    await industriesPage.softwareArticle.waitFor()
+    await industriesPage.hospitalsArticle.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 2; i++) {
-      await industriesPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await industriesPage.softwareArticle.scrollIntoViewIfNeeded()
+    await industriesPage.softwareArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await industriesPage.investimentArticle.scrollIntoViewIfNeeded()
+    await industriesPage.investimentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await industriesPage.lastForm.scrollIntoViewIfNeeded()
+    await industriesPage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await industriesPage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await industriesPage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshots on the Softeware page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
-    await industriesPage.goto('software')
-    await await page.waitForLoadState('networkidle')
+    const softwarePage = new SoftwarePage(page)
+
+    await softwarePage.goto('software')
+    await softwarePage.pageTitle.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 4; i++) {
-      await industriesPage.scrollPageWidth()
-      await await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await softwarePage.firstContentArticle.scrollIntoViewIfNeeded()
+    await softwarePage.firstContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await softwarePage.secContentArticle.scrollIntoViewIfNeeded()
+    await softwarePage.secContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await softwarePage.thirdContentArticle.scrollIntoViewIfNeeded()
+    await softwarePage.thirdContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await softwarePage.firstSolutionBlock.scrollIntoViewIfNeeded()
+    await softwarePage.firstSolutionBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await softwarePage.firstResourcesBlock.scrollIntoViewIfNeeded()
+    await softwarePage.firstResourcesBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+    
+    await softwarePage.lastForm.scrollIntoViewIfNeeded()
+    await softwarePage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await softwarePage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await softwarePage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshots on the FinTech page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
-    await industriesPage.goto('fintech')
-    await page.waitForLoadState('networkidle')
+    const finTechPage = new FinTechPage(page)
+
+    await finTechPage.goto('fintech')
+    await finTechPage.pageTitle.waitFor()
+    await finTechPage.infoBlock.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 4; i++) {
-      await industriesPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await finTechPage.firstContentArticle.scrollIntoViewIfNeeded()
+    await finTechPage.firstContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await finTechPage.secContentArticle.scrollIntoViewIfNeeded()
+    await finTechPage.secContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await finTechPage.thirdContentArticle.scrollIntoViewIfNeeded()
+    await finTechPage.thirdContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await finTechPage.firstSolutionBlock.scrollIntoViewIfNeeded()
+    await finTechPage.firstSolutionBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await finTechPage.firstResourcesBlock.scrollIntoViewIfNeeded()
+    await finTechPage.firstResourcesBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+    
+    await finTechPage.lastForm.scrollIntoViewIfNeeded()
+    await finTechPage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await finTechPage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await finTechPage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshots on the Telecom page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
-    await industriesPage.goto('telecom')
-    await page.waitForLoadState('networkidle')
+    const telecomPage = new TelecomPage(page)
+
+    await telecomPage.goto('telecom')
+    await telecomPage.pageTitle.waitFor()
+    await telecomPage.infoBlock.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 4; i++) {
-      await industriesPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await telecomPage.firstContentArticle.scrollIntoViewIfNeeded()
+    await telecomPage.firstContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await telecomPage.secContentArticle.scrollIntoViewIfNeeded()
+    await telecomPage.secContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await telecomPage.thirdContentArticle.scrollIntoViewIfNeeded()
+    await telecomPage.thirdContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await telecomPage.firstSolutionBlock.scrollIntoViewIfNeeded()
+    await telecomPage.firstSolutionBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await telecomPage.firstResourcesBlock.scrollIntoViewIfNeeded()
+    await telecomPage.firstResourcesBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+    
+    await telecomPage.lastForm.scrollIntoViewIfNeeded()
+    await telecomPage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await telecomPage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await telecomPage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshots on the Banking page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
-    await industriesPage.goto('riskManagement')
-    await page.waitForLoadState('networkidle')
+    const bankingPage = new BankingPage(page)
+
+    await bankingPage.goto('riskManagement')
+    await bankingPage.pageTitle.waitFor()
+    await bankingPage.infoBlock.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 4; i++) {
-      await industriesPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await bankingPage.firstContentArticle.scrollIntoViewIfNeeded()
+    await bankingPage.firstContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await bankingPage.secContentArticle.scrollIntoViewIfNeeded()
+    await bankingPage.secContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await bankingPage.thirdContentArticle.scrollIntoViewIfNeeded()
+    await bankingPage.thirdContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await bankingPage.firstSolutionBlock.scrollIntoViewIfNeeded()
+    await bankingPage.firstSolutionBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await bankingPage.firstResourcesBlock.scrollIntoViewIfNeeded()
+    await bankingPage.firstResourcesBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+    
+    await bankingPage.lastForm.scrollIntoViewIfNeeded()
+    await bankingPage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await bankingPage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await bankingPage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Match snapshot on the Insurance page', async ({ page }) => {
-    const industriesPage = new IndustriesPage(page)
-    await industriesPage.goto('insurance')
-    await page.waitForLoadState('networkidle')
+    const insurancePage = new InsurancePage(page)
+
+    await insurancePage.goto('insurance')
+    await insurancePage.pageTitle.waitFor()
+    await insurancePage.infoBlock.waitFor()
     expect(await page.screenshot()).toMatchSnapshot()
 
-    for(let i = 0; i < 4; i++) {
-      await industriesPage.scrollPageWidth()
-      await page.waitForLoadState('networkidle')
-      expect(await page.screenshot()).toMatchSnapshot()
-    }
+    await insurancePage.firstContentArticle.scrollIntoViewIfNeeded()
+    await insurancePage.firstContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await insurancePage.secContentArticle.scrollIntoViewIfNeeded()
+    await insurancePage.secContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await insurancePage.thirdContentArticle.scrollIntoViewIfNeeded()
+    await insurancePage.thirdContentArticle.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await insurancePage.firstSolutionBlock.scrollIntoViewIfNeeded()
+    await insurancePage.firstSolutionBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await insurancePage.firstResourcesBlock.scrollIntoViewIfNeeded()
+    await insurancePage.firstResourcesBlock.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+    
+    await insurancePage.lastForm.scrollIntoViewIfNeeded()
+    await insurancePage.lastFormImg.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
+
+    await insurancePage.reqDemoFooterBtn.scrollIntoViewIfNeeded()
+    await insurancePage.footerMenu.waitFor()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 })
